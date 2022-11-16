ee8240ec191af3fdb5d45554c626ca675d28055f
Michal Radomski <michalra@op.pl>
2022-11-16 01:10:36 +0100

    Exercise 3 -commit 2

--- 2 SQL and PostgreSQL/01 Exercises/exercise3.sql
+++ 2 SQL and PostgreSQL/01 Exercises/exercise3.sql
@@ -386,3 +386,44 @@ FROM
   JOIN photos on photos.id = comments.photo_id
   JOIN users on users.id = comments.user_id
   AND users.id = photos.user_id;
+
+select
+  user_id,
+  COUNT(id) as num_comments_created
+FROM
+  COMMENTS
+GROUP BY
+  user_id;
+
+select
+  COUNT(*)
+FROM
+  photos;
+
+select
+  user_id,
+  count(*)
+FROM
+  COMMENTS
+GROUP BY
+  user_id;
+
+select
+  photo_id,
+  count(*)
+FROM
+  COMMENTS
+group BY
+  photo_id;
+
+select
+  photo_id,
+  COUNT(*)
+FROM
+  COMMENTS
+WHERE
+  photo_id < 3
+group BY
+  photo_id
+HAVING
+  COUNT(*) > 2
