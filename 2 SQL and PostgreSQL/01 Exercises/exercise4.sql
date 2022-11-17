20edf1e2a75ad42b29cf39471a2d75bc26531e38
Michal Radomski <michalra@op.pl>
2022-11-17 01:19:37 +0100

    Exercise 4 - commit4

--- 2 SQL and PostgreSQL/01 Exercises/exercise4.sql
+++ 2 SQL and PostgreSQL/01 Exercises/exercise4.sql
@@ -133,3 +133,82 @@ WHERE
     WHERE
       department = 'Toys'
   );
+
+select
+  name,
+  price,
+  (
+    select
+      max(price)
+    FROM
+      products
+  ) as max_value
+FROM
+  products
+WHERE
+  price > 867;
+
+-- Alias: as p
+select
+  name,
+  price_weight_ratio
+FROM
+  (
+    select
+      name,
+      price / weight as price_weight_ratio
+    FROM
+      products
+  ) as p
+WHERE
+  price_weight_ratio > 5;
+
+select
+  *
+FROM
+  (
+    select
+      max(price)
+    FROM
+      products
+  ) as p;
+
+select
+  AVG(p.order_count)
+from
+  (
+    select
+      user_id,
+      count(*) as order_count
+    FROM
+      orders
+    GROUP BY
+      user_id
+  ) as p;
+
+select
+  first_name
+FROM
+  users
+  JOIN (
+    select
+      user_id
+    FROM
+      orders
+    WHERE
+      product_id = 3
+  ) as o on o.user_id = users.id;
+
+select
+  id
+FROM
+  orders
+WHERE
+  product_id IN (
+    select
+      id
+    FROM
+      products
+    WHERE
+      price / weight > 30
+  );
