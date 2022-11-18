ecad1d3612c5d82457e67813738d992a9993f341
Michal Radomski <michalra@op.pl>
2022-11-18 23:59:25 +0100

    Exercise5 done

---
+++ 2 SQL and PostgreSQL/01 Exercises/exercise5.sql
@@ -0,0 +1,47 @@
+alter table
+  products
+alter column
+  price
+set
+  not null;
+
+Alter table
+  products
+alter column
+  price
+set
+  default 999;
+
+alter table
+  products
+add
+  unique (name);
+
+alter table
+  products drop constraint products_name_key;
+
+alter table
+  products
+add
+  unique (name, department);
+
+alter table
+  products
+add
+  check (price > 0);
+
+create table orders (
+  id serial primary key,
+  name varchar(40) not null,
+  created_at Timestamp not null,
+  est_delivery timestamp not null check (created_at < est_delivery)
+);
+
+insert into
+  orders (name, created_at, est_delivery)
+values
+  (
+    'shirt',
+    '2022-nov-17 01:00AM',
+    '2022-nov-22 01:00AM'
+  );
