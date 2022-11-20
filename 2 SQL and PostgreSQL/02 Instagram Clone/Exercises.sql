59cd58a3da77983c86831ae939b0cb8e0a483f94
Michal Radomski <michalra@op.pl>
2022-11-20 19:13:54 +0100

    Instagram clone - exercises - commit3

--- 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
+++ 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
@@ -34,4 +34,45 @@ from
 select
   *
 from
+  pg_class;
+
+Create index on users (username);
+
+-- Or
+Create index usrs_username_idx on users (username);
+
+Drop index users_username_idx;
+
+explain analyze
+select
+  *
+from
+  users
+where
+  username = 'Emil30';
+
+select
+  pg_size_pretty(pg_relation_size('users'));
+
+select
+  pg_size_pretty(pg_relation_size('users_username_idx'));
+
+select
+  relname,
+  relkind
+from
   pg_class
+where
+  relkind = 'i';
+
+create extension pageinspect;
+
+select
+  *
+from
+  bt_metap('users_username_idx');
+
+select
+  *
+from
+  bt_page_items('users_username_idx', 3)
