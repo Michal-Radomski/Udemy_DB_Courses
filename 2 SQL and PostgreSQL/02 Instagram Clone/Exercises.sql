e6653437578c05a82472483fd06f7facb920b8aa
Michal Radomski <michalra@op.pl>
2022-11-21 23:58:07 +0100

    Migrations - initial commit

--- 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
+++ 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
@@ -330,4 +330,30 @@ from
 
 refresh materialized view weekly_likes;
 
--- ------------
+-- Transactions
+create table accounts (
+  id serial primary key,
+  name varchar(20) not null,
+  balance integer not null
+);
+
+insert into
+  accounts (name, balance)
+values
+  ('Gia', 100),
+  ('Alyson', 100);
+
+select
+  *
+from
+  accounts;
+
+Begin;
+
+Commit;
+
+Rollback;
+
+-- -------------------
+alter table
+  comments rename column contents to body;
