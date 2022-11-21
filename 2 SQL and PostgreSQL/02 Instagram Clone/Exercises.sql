237ff117e518b6c0186d39ae0ab8ebdea0741eee
Michal Radomski <michalra@op.pl>
2022-11-21 18:59:12 +0100

    Instagram clone - exercises - commit4

--- 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
+++ 2 SQL and PostgreSQL/02 Instagram Clone/Exercises.sql
@@ -76,3 +76,258 @@ select
   *
 from
   bt_page_items('users_username_idx', 3)
+select
+  username,
+  contents
+from
+  users
+  join comments on comments.user_id = users.id
+where
+  username = 'Alyson14';
+
+explain
+select
+  username,
+  contents
+from
+  users
+  join comments on comments.user_id = users.id
+where
+  username = 'Alyson14';
+
+explain analyze
+select
+  username,
+  contents
+from
+  users
+  join comments on comments.user_id = users.id
+where
+  username = 'Alyson14';
+
+select
+  users.username,
+  tags.created_at
+from
+  users
+  join (
+    select
+      user_id,
+      created_at
+    from
+      caption_tags
+    union
+    all
+    select
+      user_id,
+      created_at
+    from
+      photo_tags
+  ) as tags on tags.user_id = users.id
+where
+  tags.created_at < '2010-01-07';
+
+-- Below the same: Common Table Expression:
+with tags as (
+  select
+    user_id,
+    created_at
+  from
+    caption_tags
+  union
+  all
+  select
+    user_id,
+    created_at
+  from
+    photo_tags
+)
+select
+  users.username,
+  tags.created_at
+from
+  users
+  join tags as tags on tags.user_id = users.id
+where
+  tags.created_at < '2010-01-07';
+
+-- CTE - Recursive
+with recursive countdown(val) as (
+  select
+    3 as val
+  union
+  select
+    val -1
+  from
+    countdown
+  where
+    val > 1
+)
+select
+  *
+from
+  countdown;
+
+with recursive suggestions(leader_id, follower_id, depth) AS (
+  select
+    leader_id,
+    follower_id,
+    1 as depth
+  from
+    followers
+  where
+    follower_id = 1000
+  union
+  select
+    followers.leader_id,
+    followers.follower_id,
+    depth + 1
+  from
+    followers
+    join suggestions on suggestions.leader_id = followers.follower_id
+  where
+    depth < 3
+)
+select
+  distinct users.id,
+  users.username
+from
+  suggestions
+  join users on users.id = suggestions.leader_id
+where
+  depth > 1
+limit
+  5;
+
+select
+  username,
+  count(*)
+from
+  users
+  join (
+    select
+      user_id
+    from
+      photo_tags
+    union
+    all
+    select
+      user_id
+    from
+      caption_tags
+  ) as tags on tags.user_id = users.id
+group by
+  username
+order by
+  count(*) desc;
+
+-- Creating a View
+create view tags as (
+  select
+    id,
+    created_at,
+    user_id,
+    post_id,
+    'photo_tag' as type
+  from
+    photo_tags
+  union
+  all
+  select
+    id,
+    created_at,
+    user_id,
+    post_id,
+    'caption_tag' as type
+  from
+    caption_tags
+);
+
+select
+  *
+from
+  tags
+where
+  type = 'caption_tag';
+
+select
+  username,
+  count(*)
+from
+  users
+  join tags on tags.user_id = users.id
+group by
+  username
+order by
+  count(*) desc;
+
+-- ----------------
+create view recent_posts as (
+  select
+    *
+  from
+    posts
+  order by
+    created_at desc
+  limit
+    10
+);
+
+select
+  *
+from
+  recent_posts;
+
+create
+or replace view recent_posts as (
+  select
+    *
+  from
+    posts
+  order by
+    created_at desc
+  limit
+    15
+);
+
+select
+  *
+from
+  recent_posts;
+
+drop view recent_posts;
+
+-- Materialized View
+select
+  *
+from
+  likes
+  left join posts on posts.id = likes.post_id
+  left join comments on comments.id = likes.comment_id;
+
+create materialized view weekly_likes as (
+  select
+    date_trunc(
+      'week',
+      coalesce(posts.created_at, comments.created_at)
+    ) as week,
+    count(posts.id) as num_posts,
+    count (comments.id) as num_comments
+  from
+    likes
+    left join posts on posts.id = likes.post_id
+    left join comments on comments.id = likes.comment_id
+  group by
+    week
+  order by
+    week
+) with data;
+
+select
+  *
+from
+  weekly_likes;
+
+refresh materialized view weekly_likes;
+
+-- ------------
