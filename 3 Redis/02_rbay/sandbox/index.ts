64e1f2773db9288e2c2486f69fd87759cbbbaa84
Michal Radomski <michalra@op.pl>
2024-12-10 00:33:11 +0100

    Redis course - commit 07

--- 3 Redis/02_rbay/sandbox/index.ts
+++ 3 Redis/02_rbay/sandbox/index.ts
@@ -1,5 +1,20 @@
 import "dotenv/config";
+
 import { client } from "../src/services/redis";
 
-const run = async () => {};
+const run = async (): Promise<void> => {
+  await client.hSet("car2", {
+    color: "red",
+    year: 1950,
+  });
+
+  const car2 = await client.hGetAll("car2");
+
+  if (Object.keys(car2).length === 0) {
+    console.log("Car not found, respond with 404");
+    return;
+  }
+
+  console.log("car2:", car2);
+};
 run();
