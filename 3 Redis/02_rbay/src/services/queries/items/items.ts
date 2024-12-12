cafb62617d5012fd94e5052bf16132a7ff123afd
Michal Radomski <michalra@op.pl>
2024-12-12 01:31:26 +0100

    Redis course - commit 15

--- 3 Redis/02_rbay/src/services/queries/items/items.ts
+++ 3 Redis/02_rbay/src/services/queries/items/items.ts
@@ -1,4 +1,4 @@
-import type { CreateItemAttrs } from "$services/types";
+import type { CreateItemAttrs, Item } from "$services/types";
 import { client } from "$services/redis";
 import { serialize } from "./serialize";
 import { genId } from "$services/utils";
@@ -15,7 +15,21 @@ export const getItem = async (id: string) => {
   return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]): Promise<Item[]> => {
+  const commands = ids.map((id) => {
+    return client.hGetAll(itemsKey(id));
+  });
+
+  const results = await Promise.all(commands);
+
+  return results.map((result, i) => {
+    if (Object.keys(result).length === 0) {
+      return null;
+    }
+
+    return deserialize(ids[i], result);
+  });
+};
 
 export const createItem = async (attrs: CreateItemAttrs) => {
   const id = genId();
