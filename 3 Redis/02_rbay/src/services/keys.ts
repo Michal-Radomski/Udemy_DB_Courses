6891e0792d2e846c732f69b184bbd84b2ac27fb9
Michal Radomski <michalra@op.pl>
2024-12-12 22:58:28 +0100

    Redis course - commit 17

--- 3 Redis/02_rbay/src/services/keys.ts
+++ 3 Redis/02_rbay/src/services/keys.ts
@@ -5,3 +5,7 @@ export const usersKey = (userId: string): string => `users#${userId}`;
 export const sessionsKey = (sessionId: string): string => `sessions#${sessionId}`;
 
 export const itemsKey = (itemId: string): string => `items#${itemId}`;
+
+export const usernamesUniqueKey = (): string => "usernames:unique";
+
+export const userLikesKey = (userId: string): string => `users:likes#${userId}`;
