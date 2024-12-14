7d5884736dd5288f0ab978b2af3c0bae4258b083
Michal Radomski <michalra@op.pl>
2024-12-14 19:59:53 +0100

    Redis course - commit 25

--- 3 Redis/02_rbay/src/services/queries/items/by-ending-time.ts
+++ 3 Redis/02_rbay/src/services/queries/items/by-ending-time.ts
@@ -2,14 +2,19 @@ import { client } from "$services/redis";
 import { itemsKey, itemsByEndingAtKey } from "$services/keys";
 import { deserialize } from "./deserialize";
 
+//* Error here?
 export const itemsByEndingTime = async (order: "DESC" | "ASC" = "DESC", offset = 0, count = 10) => {
-  const ids = await client.zRange(itemsByEndingAtKey(), Date.now(), "+inf", {
-    BY: "SCORE",
+  const ids = await client.zRangeByScore(itemsByEndingAtKey(), "-inf", "+inf", {
+    // BY: 'SCORE',
     LIMIT: {
       offset,
       count,
     },
   });
+  // console.log({ ids });
 
-  console.log({ ids });
+  const results = await Promise.all(ids?.map((id) => client?.hGetAll(itemsKey(id))));
+  // console.log("results:", results);
+
+  return results.map((item, i) => deserialize(ids[i], item));
 };
