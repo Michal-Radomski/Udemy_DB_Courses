63cf297d2afe49ce1a9320c09decc3a9f363e472
Michal Radomski <michalra@op.pl>
2024-12-16 00:52:11 +0100

    Redis course - commit 38

--- 3 Redis/02_rbay/src/services/redis/client.ts
+++ 3 Redis/02_rbay/src/services/redis/client.ts
@@ -1,4 +1,6 @@
-import { createClient } from "redis";
+import { createClient, defineScript } from "redis";
+
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from "$services/keys";
 
 const client = createClient({
   socket: {
@@ -6,6 +8,47 @@ const client = createClient({
     port: parseInt(process.env.REDIS_PORT),
   },
   password: process.env.REDIS_PW,
+  scripts: {
+    addOneAndStore: defineScript({
+      NUMBER_OF_KEYS: 1,
+      SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+      transformArguments(key: string, value: number) {
+        return [key, value.toString()];
+      },
+      transformReply(reply: any) {
+        return reply;
+      },
+    }),
+    incrementView: defineScript({
+      NUMBER_OF_KEYS: 3,
+      SCRIPT: `
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`,
+      transformArguments(itemId: string, userId: string) {
+        return [itemsViewsKey(itemId), itemsKey(itemId), itemsByViewsKey(), itemId, userId];
+      },
+      transformReply() {},
+    }),
+  },
+});
+
+client.on("connect", async () => {
+  await client.addOneAndStore("books:count", 10);
+  const result = await client.get("books:count");
+  console.log("result:", result);
 });
 
 client.on("error", (err) => console.error(err));
